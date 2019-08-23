800aadf9a23731626977017ceab75883fbf07d48
chenhnu <1640768142@qq.com>
2019-08-23 15:53:20 +0800

    Update deque.go

--- ptcollection/deque.go
+++ ptcollection/deque.go
@@ -7,30 +7,67 @@ import (
 	"github.com/chenhnu/go-lib/ptlog"
 )
 
-type Deque struct {
-	head      int
-	tail      int
+type deque struct {
+	head      *interface{}
+	tail      *interface{}
 	size      int
 	container []interface{}
 }
 
-func NewDeque(args ...interface{}) Deque {
-	return Deque{
-		head:      0,
-		tail:      len(args) - 1,
+func NewDeque(args ...interface{}) *deque {
+	if args == nil {
+		return &deque{}
+	}
+	return &deque{
+		head:      &args[0],
+		tail:      &args[len(args)-1],
 		size:      len(args),
 		container: args,
 	}
 }
-
-func (deque Deque)LPush(value interface{}) interface{} {
-	if deque.size==0{
+func (deq *deque) GetContent() []interface{} {
+	return deq.container
+}
+func (deq *deque) IsEmpty() bool {
+	if deq.size == 0 {
+		return true
+	}
+	return false
+}
+func (deq *deque) Size() int {
+	return deq.size
+}
+func (deq *deque) LPop() interface{} {
+	if deq.size == 0 {
 		ptlog.Error(errors.New("deque is null"))
 		return nil
 	}
-	deque.tail--
-	temp:=deque.container[0]
-	copy(deque.container,deque.container[1:])
-	deque.size--
-	return temp
+	res := &deq.head
+	deq.container = deq.container[1:]
+	deq.size--
+	deq.head = &deq.container[0]
+	return res
+}
+func (deq *deque) RPop() interface{} {
+	if deq.size == 0 {
+		ptlog.Error(errors.New("deque is null"))
+		return nil
+	}
+	res := &deq.tail
+	deq.container = deq.container[0 : deq.size-1]
+	deq.size--
+	deq.tail = &deq.container[deq.size-1]
+	return res
+}
+
+func (deq *deque) LPush(value interface{}) {
+	deq.head = &value
+	deq.size++
+	tem := []interface{}{value}
+	deq.container = append(tem, deq.container...)
+}
+func (deq *deque) RPush(value interface{}) {
+	deq.tail = &value
+	deq.size++
+	deq.container = append(deq.container, value)
 }
